51e701e77c9988a5886b92783bbc77a7f6ee0fb4
Klae Eckhold (1000135705) <ECKHKL1@student.op.ac.nz>
2026-05-14 16:37:23 +1200

    Code recovered

--- Program.cs
+++ Program.cs
@@ -2,25 +2,25 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             string userInput;
             string result;
             Console.WriteLine("You are in a dark and forbidding place");
             Console.WriteLine("what do you want to do?");       
             userInput = Console.ReadLine();
-            if userInput == "quit"
+            if (userInput == "quit")
             {
                 Console.WriteLine("You quit the game");
             }
-            else if userInput == "help"
+            else if (userInput == "help")
             {
-                Console.WriteLine("type the following to exit the game: quit")
+                Console.WriteLine("type the following to exit the game: quit");
             }
             else
             {
-                Console.WriteLine("go away")
-                Thread.Sleep(1000)
+                Console.WriteLine("go away");
+                Thread.Sleep(1000);
             }
         }
     }
