b31d6d7b49593d77939b27bc00ecdbcbbdaaa6ff
Klae Eckhold (1000135705) <ECKHKL1@student.op.ac.nz>
2026-05-14 16:07:15 +1200

    Updated ReadMe File

--- obj/Debug/net8.0/NewGame.AssemblyInfo.cs
+++ obj/Debug/net8.0/NewGame.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NewGame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8f69e22c1b70b9839648984507bed981656cf08")]
 [assembly: System.Reflection.AssemblyProductAttribute("NewGame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NewGame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
