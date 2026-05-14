1d3c83b9966f0c32a7e1e629e49eb221025eb40d
Antony van der Schuit (1000144772) <VANDAJ8@student.op.ac.nz>
2026-05-14 16:33:19 +1200

    first day basic code

--- Program.cs
+++ Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string userInput;
+            string result;
+            Console.WriteLine("You are in a dark and forbidding place");
+            Console.WriteLine("what do you want to do?");       
+            userInput = Console.ReadLine();
+            if userInput == "quit"
+            {
+                Console.WriteLine("You quit the game");
+            }
+            else if userInput == "help"
+            {
+                Console.WriteLine("type the following to exit the game: quit")
+            }
+            else
+            {
+                Console.WriteLine("go away")
+                Thread.Sleep(1000)
+            }
         }
     }
 }
