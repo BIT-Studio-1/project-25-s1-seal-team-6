85f00b34f5e1f8f58b872e67a9bee22aa3376c9c
Alfie McCormack <mccoar1@student.op.ac.nz>
2026-08-02 11:41:49 +1200

    Changed the meadow colour, even though it isnt currently used

--- Program.cs
+++ Program.cs
@@ -89,9 +89,11 @@ namespace NewGame
             Console.Clear();
 
         }
-        public static void Meadow()
-        {
+        public static void Meadow()//Unused. Chnaged colours anyway
+        {            
+            Console.ForegroundColor = ConsoleColor.DarkGray;
             Console.WriteLine("        ______\r\n       /     /\\\r\n      /     /  \\\r\n     /_____/----\\_    (  \r\n    \"     \"          ).  \r\n   _ ___          o (:') o   \r\n  (@))_))        o ~/~~\\~ o   \r\n                  o  o  o\r\n");
+            Console.ResetColor();
         }
 
         public static void Pending_Bonfire()
